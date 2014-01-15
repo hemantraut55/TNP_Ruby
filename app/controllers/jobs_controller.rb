ff596ee3cbad1a916e9924bb2b1701b067d234f2
hemant <hemant@cloudvisionsys.com>
2014-01-15 20:03:05 +0530

    job post all field entered

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,6 +10,7 @@ class JobsController < ApplicationController
   end
   private 
   def post_params
-  params.require(:postjob).permit(:company_id,:profile_type,:designation,:salary,:job_location)
+  params.require(:postjob).permit(:company_id,:profile_type,:designation,:salary,:job_location,:venue,:interview_date,:last_date,:company_profile,:job_details,:selection_process,:passout_year,:bond_period,
+  :branch,:degree,:hsc_mark,:ssc_mark,:graduation,:post_graduation,:live_backlog,:dead_backlog,:yd,:already_offer)
   end
 end
